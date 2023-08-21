040d7c36613fffde1e5b35c1b1901c218c749681
Nahuel Burgueño <nahuelburge@gmail.com>
2023-08-21 20:33:50 -0300

    Nuevas funcionalidades
    Creando nuevo modelo, nuevas vistas, agregar proyecto

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -2,17 +2,24 @@
 
 namespace Controllers;
 
+use Model\Proyecto;
 use MVC\Router;
+
 
 class DashboardController {
     public static function index(Router $router) {
 
         session_start();
-
         isAuth();
+
+        $id = $_SESSION['id'];
+        $proyectos = Proyecto::belongsTo('propietarioId', $id);
+
+
         
         $router->render('dashboard/index', [
-            'titulo' => 'Proyectos'
+            'titulo' => 'Proyectos',
+            'proyectos' => $proyectos
         ]);
     }
 
@@ -21,10 +28,54 @@ class DashboardController {
         isAuth();
         $alertas = [];
 
+        if($_SERVER["REQUEST_METHOD"] === 'POST' ){
+            $proyecto = new Proyecto($_POST);
+
+            // Validacion
+            $alertas = $proyecto->validarProyecto();
+
+            if(empty($alertas)) {
+
+                //Generar una url unica
+                $hash = md5(uniqid());
+                $proyecto->url = $hash;
+
+                //Almacenar el creador del proyecto
+                $proyecto->propietarioId = $_SESSION['id'];
+
+                // Guardar el proyecto
+                $proyecto->guardar();
+
+                // Redireccionar
+                header('Location: /proyecto?id=' . $proyecto->url);
+            }
+            
+        }
+
         $router->render('dashboard/crear-proyecto', [
             'alertas' => $alertas,
             'titulo' => 'Crear Proyecto'
             
+        ]);
+    }
+
+    public static function proyecto(Router $router) {
+        session_start();
+        isAuth();
+
+        
+        $token = $_GET['id'];
+        if(!$token) header('Location: /dashboard');
+
+        // Revisar que la persona que visita el proyecto, es quen la creo
+        $proyecto = Proyecto::where('url', $token);
+
+        if($proyecto->propietarioId !== $_SESSION['id']) {
+            header('Location: /dashboard');
+        }
+        
+        $router->render('dashboard/proyecto', [
+            'titulo' => $proyecto->proyecto
         ]);
     }
 
